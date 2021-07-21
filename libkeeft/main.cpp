4f82af6c680937d917bca3a9a762f502082338fa
ReneMuala <renemuala@icloud.com>
2021-07-21 16:05:57 +0200

    Initial Commit

---
+++ libkeeft/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  libkeeft
+//
+//  Created by René Descartes Domingos Muala on 21/07/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
